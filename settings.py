cf2a3e91ed540ade93c639ceb8ba22108552d6a8
Aisha Kaliel <aisha.kaliel@gmail.com>
2012-03-04 14:40:52 -0800

    adding admin template overrides and fixing settings

--- settings.py
+++ settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/PATH/TO/SITE/static'
+    '/PATH/TO/SITE/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -107,7 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/PATH/TO/SITE/templates'
+    '/PATH/TO/SITE/templates',
 )
 
 INSTALLED_APPS = (
