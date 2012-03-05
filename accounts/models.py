ac0ee978c3942dbde19e5e642ed8b60fa39b9d9c
Aisha Kaliel <aisha.kaliel@gmail.com>
2012-03-04 18:30:35 -0800

    oops, need to set account fields to blank so not required

--- accounts/models.py
+++ accounts/models.py
@@ -10,14 +10,14 @@ class AuthorProfile(models.Model):
     
     avatar = models.ImageField(upload_to='avatars')
     description = models.TextField()
-    site = models.URLField(null=True)
+    site = models.URLField(null=True, blank=True)
     
     #Social links
-    facebook = models.CharField(max_length=200, null=True)
-    googleplus = models.CharField(max_length=200, null=True)
-    twitter = models.CharField(max_length=200, null=True)
-    tumblr = models.URLField(null=True)
-    pinterest = models.CharField(max_length=200, null=True)
+    facebook = models.CharField(max_length=200, null=True, blank=True)
+    googleplus = models.CharField(max_length=200, null=True, blank=True)
+    twitter = models.CharField(max_length=200, null=True, blank=True)
+    tumblr = models.URLField(null=True, blank=True)
+    pinterest = models.CharField(max_length=200, null=True, blank=True)
 
     class Meta:
       app_label = 'accounts'
